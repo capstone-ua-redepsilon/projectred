a05ef67ee5a907fc5f89ba4fd2d89d8179457ecf
Tyler Johnson <tsjnsn@gmail.com>
2014-02-11 15:23:04 -0600

    first commit

---
+++ src/com/uacapstone/red/LandingActivity.java
@@ -0,0 +1,38 @@
+package com.uacapstone.red;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LandingActivity extends Activity {
+
+	// Listeners
+	final OnClickListener clickStart = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			setContentView(R.layout.activity_game);
+		}
+	};
+	
+	// Main Code
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_landing);
+		
+		Button btnStart = (Button) findViewById(R.id.btnStart);
+		
+		btnStart.setOnClickListener(clickStart);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.landing, menu);
+		return true;
+	}
+
+}
