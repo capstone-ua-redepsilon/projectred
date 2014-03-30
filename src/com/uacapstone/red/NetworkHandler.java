06164c11c56ba0906d387394b52bb3321738da5b
Tyler Johnson <tsjnsn@gmail.com>
2014-03-30 00:33:36 -0500

    z

--- src/com/uacapstone/red/NetworkHandler.java
+++ src/com/uacapstone/red/NetworkHandler.java
@@ -39,7 +39,6 @@ public class NetworkHandler {
 	
 	public void testZMQ() {
 		ZMQ.Context context = ZMQ.context(1);
-	//  Socket to talk to server
         System.out.println("Connecting to hello world server…");
 
         ZMQ.Socket requester = context.socket(ZMQ.REQ);
@@ -47,7 +46,6 @@ public class NetworkHandler {
 
         for (int requestNbr = 0; requestNbr != 10; requestNbr++) {
             String request = "Hello";
-            System.out.println("Sending Hello " + requestNbr);
             requester.send(request.getBytes(), 0);
 
             byte[] reply = requester.recv(0);
